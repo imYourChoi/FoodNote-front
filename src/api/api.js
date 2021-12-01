6df4b64bb08587bef3086f9dc9474fcad65a6827
JeeHun <jeehun@kaist.ac.kr>
2021-12-02 01:29:26 +0900

    Fix .env finally plz

--- src/api/api.js
+++ src/api/api.js
@@ -5,20 +5,20 @@ const backAddress = process.env.REACT_APP_API_URL;
 console.log(backAddress);
 
 export const add = (data) => {
-  axios.post(`${backAddress}/api/food`, {
+  axios.post(`${backAddress}/food`, {
     restaurant: data.restaurant,
     food: data.food,
     date: data.date,
   });
 };
 
-export const getAll = () => axios.get(`${backAddress}/api/food`);
+export const getAll = () => axios.get(`${backAddress}/food`);
 
 export const deleteOne = (item) =>
-  axios.delete(`${backAddress}/api/food/${item._id}`);
+  axios.delete(`${backAddress}/food/${item._id}`);
 
 export const update = (id, data) =>
-  axios.put(`${backAddress}/api/food/${id}`, {
+  axios.put(`${backAddress}/food/${id}`, {
     restaurant: data.restaurant,
     food: data.food,
   });
